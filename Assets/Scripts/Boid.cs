b779e3f69b9ba89e2bf09c101048887c687d6053
nukokrenard <skeanobo@gmail.com>
2026-02-16 20:54:04 -0500

    Added main boid script and UI Elements

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class Boid : MonoBehaviour
 {
@@ -7,23 +8,51 @@ public class Boid : MonoBehaviour
     public float collisionDistance;
     public List<Boid> boids;
     public Boid myScript;
-
+    public float directional_noise;
+    public int distance_falloff_exponent;
+    public float lifetime;
     void Start()
     {
-        myScript = GetComponent<Boid>();	    
+        myScript = GetComponent<Boid>();
+        if (lifetime == -1)
+	{
+		lifetime *= 100;
+	}	
     }
     void Update()
     {
+	if (lifetime < 0 && lifetime > -100)
+	{
+		boids.Remove(myScript);
+		Destroy(gameObject);
+	}
+	lifetime -= Time.deltaTime;
 	bool will_collide = false;
 
        Vector2 collisionsCenter = new Vector2(0,0);
        int collisionsCount = 0;
+
+       Vector2 flockPosition = new Vector2(0,0);
+       Vector2 flockHeading = new Vector2(0,0);
+
+       Vector2 screen_size = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,Screen.height));
        foreach (Boid boid in boids)
        {
+
 	       if (boid != myScript)
 	       {
-	           Vector2 direction = boid.transform.position-transform.position;
-		   float distance = direction.magnitude;
+		       Vector2 direction = boid.transform.position-transform.position;
+		       float distance = direction.magnitude;
+	      
+		       //Adds to the average flock position, weighted inversely by the boid's distance.
+		       //The weight also has to take into account the portal effect on each side of the screen
+
+		       flockPosition += (Vector2)(direction/boids.Count)*DistanceFalloff(boid,screen_size); 
+		       flockHeading += (Vector2)(boid.transform.up/boids.Count)*DistanceFalloff(boid,screen_size);
+			       
+					
+	       
+
 		   if (distance < collisionDistance)
 		   {
 			   collisionsCount++;
@@ -34,21 +63,38 @@ public class Boid : MonoBehaviour
        }
        collisionsCenter/=collisionsCount;
        collisionsCenter = collisionsCenter.normalized;
-       if (
-				  transform.position.x > 10 ||
-				  transform.position.y > 10 ||
-				  transform.position.x < 0 ||
-				  transform.position.y < 0)
-		  {
-			  //HeadTowards(-transform.position);
-		  }
-		  else if (will_collide)
-		  {
-			  Debug.Log("Will collide with: "+collisionsCenter);
-			  HeadAway(collisionsCenter);
-		  }
+       if (transform.position.y < -screen_size.y && transform.up.y < 0)
+       {
+	       transform.position = new Vector2(transform.position.x,screen_size.y);
+       }
+       else if (transform.position.y > screen_size.y && transform.up.y > 0)
+       {
+	       transform.position = new Vector2(transform.position.x,-screen_size.y);
+       }
+       if (transform.position.x < -screen_size.x && transform.up.x < 0)
+       {
+	       transform.position = new Vector2(screen_size.x,transform.position.y);	       
+       }
+       else if (transform.position.x > screen_size.x && transform.up.x > 0)
+       {
+	       transform.position = new Vector2(-screen_size.x,transform.position.y);
+       }
 
-      // transform.position += transform.up*Time.deltaTime*speed;
+
+	       
+       
+       if (will_collide)
+       {
+	       HeadAway(collisionsCenter);
+       }
+       else 
+       {
+	       HeadTowards(flockPosition);
+	       HeadTowards(flockHeading);
+       }
+
+       transform.localEulerAngles += new Vector3(0,0,Random.Range(-directional_noise,directional_noise));
+       transform.position += transform.up*Time.deltaTime*speed;
 
     }
 
@@ -91,5 +137,24 @@ public class Boid : MonoBehaviour
 	    float y = Mathf.Cos(angle)*start.y-Mathf.Sin(angle)*start.x;
 
 	    return new Vector2(x,y);
+    }
+
+    Vector2 DistanceFalloff(Boid boid, Vector2 screen_size)
+    {
+	    //This part uses a sin function to repeat the distance, so that the weights repeat at the edges.
+	    return new Vector2(
+			    //Depending on the exponent, Cosine has the ability to return a negative value, this is because if the boid we are calculating for distance is on the bottom of the screen, I want the boid distance equasion to invert it's direction.
+			    //This way boids understand the wrapping effect of the game space, and (for example one boid is on the bottom of the screen and one is on the top, instead of taking the long way and going to the middle, they will go to the ends of the screen so they can wrap around to each other.
+			    //Unfortunately this seems to make the Boids really want to stick to the edges, I do not know why this bug happens, but it can be fixed by setting the distance exponent to an even number (making all negative numbers positive)
+			    /*
+			     *I have a theory as to why this happens. The main goal of the boids is to try and flock together, and because the map wraps around they are not only attracted to nearby boids, but also if a boid is on the edge they are attracted to a boid on the opposing edge.
+			     * Originally the attraction was negative so that the boid would take the quickest route (off of the side of the frame so it wraps around) to the group of boids.
+			     * The weight of a boids position and heading can be imagined as a cube made of two cosine functions, with the period of cone cosine function being twice the size of the screen (in either width or height) So if a boid is at the edge you can imagine that all of the area nearby the boid in a square has a high weight, and will have a large impact on the boid, however the boids on the other side would also have a weight, so the square repeats.
+			     * I believe that the reason boids are attracted to the edges when given an odd exponent is because it is at the edges of the screen when a boid has the hights total weight across the screen, and because of the variety of directions the total direction and position is more stable and centered.
+			     * */
+			    //Technically the boids are not supposed to do this, but it looks pretty so I'm keeping it in.
+			    Mathf.Pow((Mathf.Cos(((boid.transform.position.x-transform.position.x)/(2*screen_size.x)*Mathf.PI))),distance_falloff_exponent),
+	                    Mathf.Pow((Mathf.Cos(((boid.transform.position.y-transform.position.y)/(2*screen_size.y)*Mathf.PI))),distance_falloff_exponent)
+			    );
     }
 }
--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro; 
 
 public class BoidController : MonoBehaviour
 {
@@ -7,7 +9,13 @@ public class BoidController : MonoBehaviour
     public float moveAwayDistance = 1.0f;
     public GameObject prefab;
     public List<Boid> boids;
-
+    public float noise;
+    public int distance_falloff_exponent;
+    public Slider NoiseSlider;
+    public Slider FalloffSlider;
+    public Slider LifeTimeSlider;
+    public TMP_Text BoidsCounter; 
+    public TMP_Text FrameRate;
     void Start()
     {
 	    boids = new List<Boid>();
@@ -16,17 +24,41 @@ public class BoidController : MonoBehaviour
 
     void Update()
     {
-	    Debug.Log(Mathf.Atan2(transform.up.x,transform.up.y));
+	    BoidsCounter.text = "Boids: "+boids.Count;
+	    FrameRate.text = "FPS: "+(1/Time.deltaTime);
     }
 
-    public void CreateBoid() 
+    public void CreateBoid(int count) 
     {
-	    Boid boid = Instantiate(prefab,transform).GetComponent<Boid>();
-
-	    boids.Add(boid);
-	    boid.boids = boids;
-	    boid.speed = boidSpeed;
-	    boid.collisionDistance = moveAwayDistance;
+	    for (int i = 0; i < count; i ++)
+	    {
+		    Boid boid = Instantiate(prefab,transform).GetComponent<Boid>();
+		    boids.Add(boid);
+		    boid.boids = boids;
+		    boid.speed = boidSpeed;
+		    boid.collisionDistance = moveAwayDistance;
+		    boid.lifetime = LifeTimeSlider.value;
+		    boid.directional_noise = noise;
+		    boid.distance_falloff_exponent = distance_falloff_exponent;
+	    }
     }
+    
+    public void changeBoidNoise()
+     {
+             noise = NoiseSlider.value;
+ 
+             foreach (Boid boid in boids)
+             {
+                     boid.directional_noise = noise;
+             }
+     }
 
+    public void changeExponentFalloff()
+    {
+	    distance_falloff_exponent = (int)FalloffSlider.value;
+	    foreach (Boid boid in boids)
+	    {
+		    boid.distance_falloff_exponent = distance_falloff_exponent;
+	    }
+    }
 }
